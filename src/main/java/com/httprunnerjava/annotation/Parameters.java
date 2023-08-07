5354c58a4b20f5a8f0c654213f96b06f99e8fddb
chucan <chucan@qunhemail.com>
2023-08-08 00:34:04 +0800

    修改注释内容

--- src/main/java/com/httprunnerjava/annotation/Parameters.java
+++ src/main/java/com/httprunnerjava/annotation/Parameters.java
@@ -10,20 +10,21 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Parameters {
 
-    /**从文件中加载map待实现，但是这里限定只能加载csv类型的文件，且文件第一行需要是标题
-     * csvFile指的是待加载文件的路径，从根节点开始
+    /**
+     * 从文件中加载map，但是这里限定只能加载csv类型的文件，且文件第一行需要是标题（字段名）
+     * csvFile指的是待加载文件的绝对路径，从项目的根节点开始
      */
     String csvFile() default "";
 
-    /* mapStr可以是如下类型的字符串
-        {
-            "param1":"5SFXXXXXXXX",
-            "param2":12345,
-            "param3":["foo1","foo2"],
-            "param4":["soo1","soo2"]
-        }，
-     也可以是一个函数${function{param1,param2)}，但是函数的返回值一定是上述格式，否则会产生意料之外的结果
+    /**
+     * mapStr可以是如下类型的字符串
+     *  {
+     *       "param1":"5SFXXXXXXXX",
+     *       "param2":12345,
+     *       "param3":["foo1","foo2"],
+     *       "param4":["soo1","soo2"]
+     *   }，
+     * 也可以是一个函数${function{param1,param2)}，但是函数的返回值一定是上述格式，否则会产生意料之外的结果
      */
     String mapStr() default "";
-
 }
