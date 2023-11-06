fdf74d4ea8a6c3403f02f5cd423966b31f0a2534
chucan <chucan@qunhemail.com>
2023-11-06 17:35:09 +0800

    优化重试时间

--- src/main/java/com/httprunnerjava/RetryAnalyzer.java
+++ src/main/java/com/httprunnerjava/RetryAnalyzer.java
@@ -5,10 +5,15 @@ import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
     private int retryCount = 0;
-    private static final int MAX_RETRY_COUNT = 1;
+    private static final int MAX_RETRY_COUNT = 2;
 
     @Override
     public boolean retry(ITestResult result) {
+        // 失败重试时，默认等待10秒，让服务缓一缓
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+        }
         if (retryCount < MAX_RETRY_COUNT) {
             retryCount++;
             return true;
